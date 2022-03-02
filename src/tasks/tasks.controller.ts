c01d4d028ed1c7e28d1e15b680403b0ff6bc9afc
Aksa@4321 <ghazan.usman@aksa-sds.com>
2022-03-02 17:57:39 +0500

    added postgress

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,35 +6,32 @@ import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor( private _taskService: TasksService){
+  constructor(private _taskService: TasksService) {}
 
-    }
+  @Get()
+  getAllTasks(): Tasks[] {
+    return this._taskService.getAllTasks();
+  }
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Tasks {
+    return this._taskService.createTask(createTaskDto);
+  }
 
-    @Get()
-    getAllTasks(): Tasks[] {
-       return this._taskService.getAllTasks();
-    }
-    @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto): Tasks {
-        
-       return this._taskService.createTask(createTaskDto);
-    }
+  @Get('/:id')
+  getTaskByID(@Param('id') id: string): Tasks {
+    return this._taskService.getTaskByID(id);
+  }
 
-    @Get('/:id')
-    getTaskByID(@Param('id') id: string): Tasks{
-       return this._taskService.getTaskByID(id);
- 
-    }
+  @Delete('/:id')
+  deleteTaskByID(@Param('id') id: string): void {
+    return this._taskService.deleteTaskByID(id);
+  }
 
-    @Delete('/:id')
-    deleteTaskByID(@Param('id') id: string): void{
-       return this._taskService.deleteTaskByID(id);
- 
-    }
-
-    @Delete('/:id/status')
-    updateTaskStatus(@Param('id') id: string , @Body('status') status: TasksStatus): Tasks{
-       return this._taskService.updateTaskStatus(id,status);
- 
-    }
+  @Delete('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TasksStatus,
+  ): Tasks {
+    return this._taskService.updateTaskStatus(id, status);
+  }
 }
